8b8315a5cf9a604cdc5fc2ace426485446b225ba
ex <ex@devtal.de>
2016-06-07 13:25:20 +0200

    select ways

--- project/bad/conf.php
+++ project/bad/conf.php
@@ -1,7 +1,7 @@
 <?php
 include __DIR__ . "/../default/conf.php";
 
-$conf['data_url']		= '[out:json];area["name"="Deutschland"]["boundary"="administrative"];(node["sport"="swimming"](area);node["leisure"="water_park"](area););out;';
+$conf['data_url']		= '[out:json];area["name"="Deutschland"]["boundary"="administrative"];(node["sport"="swimming"](area);way["sport"="swimming"](area);node["leisure"="water_park"](area));out;';
 $conf['lat']                    = "51.159";
 $conf['lon']                    = "9.976";
 $conf['zoom']                   = "6";
