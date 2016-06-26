30e8cc30a71c3d1fe297a3aae573a41400d6a332
ex <ex@devtal.de>
2016-06-26 11:12:40 +0200

    _blank

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ include $conf_file;
       } else {
         if( typeof node.tags.name !== "undefined" ) popup += node.tags.name;
         if( typeof node.tags['website'] !== "undefined" ) popup += "<br/><a href=\"" + getUrl( node.tags['website'] ) + "\" target=\"_blank\">Website</a><br/>"; 
-        popup += "<br/><a href=\"http://www.openstreetmap.org/edit?node=" + node.id + "\">edit</a>";
+        popup += "<br/><a href=\"http://www.openstreetmap.org/edit?node=" + node.id + "\" target=\"_blank\">edit</a>";
       }
       L.marker([node.lat, node.lon], {"title": title, "icon": icon}).bindPopup(popup).addTo(markers);
       }
