3102e92de6ea5edd53fcba83b9c3549e9973a75b
ex <ex@devtal.de>
2016-06-26 17:56:20 +0200

    enabled piwik

--- index.php
+++ index.php
@@ -170,4 +170,24 @@ foreach($projects as $project)
   </div>
 
   <div id="map"></div>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.kartenkarte.org"]);
+  _paq.push(["setDomains", ["*.kartenkarte.org"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.nico-hessler.de/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 8]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//stats.nico-hessler.de/piwik/piwik.php?idsite=8" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
 </body>
