ed6b52f884dc00db38f5173f926a484a724d98ae
ex <ex@devtal.de>
2016-07-12 11:47:55 +0200

    more arrenberg

--- project/arrenberg/conf.php
+++ project/arrenberg/conf.php
@@ -2,8 +2,12 @@
 include __DIR__ . "/../default/conf.php";
 
 $conf['data_url']		= '[out:json];area["postal_code"="42117"]["boundary"="postal_code"]->.boundaryarea;(
-node(area.boundaryarea)["amenity"~"restaurant"];
-node(area.boundaryarea)["highway"~"bus_stop"];
+node(area.boundaryarea)["shop"];
+node(area.boundaryarea)["amenity"="restaurant"];
+node(area.boundaryarea)["highway"="bus_stop"];
+node(area.boundaryarea)["shop"="bakery"];
+node(area.boundaryarea)["leisure"="garden"];
+node(area.boundaryarea)["leisure"="playground"];
 >;);out;';
 $conf['data_url']               = trim(preg_replace('/\\n/', '', $conf['data_url']));
 $conf['lat']                    = "51.2411";
