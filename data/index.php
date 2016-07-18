26d4a5bd18ef68fe22e87b6b261efcc180401f8b
ex <ex@devtal.de>
2016-07-18 18:48:29 +0200

    cache json

--- data/index.php
+++ data/index.php
@@ -5,7 +5,20 @@
 header("Content-Encoding: gzip");
 header('Content-type: application/json');
 
-$compressed = gzcompress( file_get_contents( basename($_GET['file']) ) );
+$file =  basename($_GET['file']);
+
+$last_modified_time = filemtime( $file );
+$etag = md5_file( $file );
+header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT"); 
+header("Etag: $etag"); 
+
+if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time || 
+    @trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) { 
+    header("HTTP/1.1 304 Not Modified"); 
+    exit; 
+}
+
+$compressed = gzcompress( file_get_contents( $file ) );
 echo "\x1f\x8b\x08\x00\x00\x00\x00\x00";
 echo $compressed;
 
